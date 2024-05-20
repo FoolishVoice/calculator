2671c853a26da66b451d4928724426ab2a885ff2
FoolishVoice <158341790+FoolishVoice@users.noreply.github.com>
2024-05-20 15:52:31 -0400

    Add files to make a calculator

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+const firstNumber = '';
+const secondNumber = '';
+const operator = '';
+
+function operate(a, b, operator) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    }
+}
+
+
+const calc = document.querySelector('.calc');
